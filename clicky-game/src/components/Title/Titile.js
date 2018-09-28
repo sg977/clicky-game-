caf1211d61ab2c932fb199aad9a9f1833d7d8462
Siyi Guo <guosiyi1993@gmail.com>
2018-09-27 21:49:22 -0400

    final draft, format needed to be fixed

---
+++ clicky-game/src/components/Title/Titile.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Title.css"; 
+
+const Title = props => (
+    <div className="titleBox">
+    <h1>Clicky Game!</h1>
+    <p>Click on an image to earn points, but do not click on any more than once!</p>
+</div>
+
+
+);
+
+export default Title;
+
